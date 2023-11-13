a4431e367671879e5c3ca737ba87b5db62a7228b
UzomaAkpala <francisakpala@yahoo.com>
2023-11-13 13:20:46 +0100

    basic express setup

--- app.js
+++ app.js
@@ -1,53 +1,19 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
 
-//get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  //home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  }
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>This is our About Page</h1>");
-    res.end();
-  }
-
-  // styles
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  }
-
-  // image/logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  }
-
-  // logic
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  }
-  // 404
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>page not found</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.status(200).send("Home page");
 });
 
-server.listen(5000, console.log("Server is listening on port 5000..."));
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000...");
+});
