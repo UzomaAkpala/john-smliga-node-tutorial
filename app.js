16ac4fe7c9788284f2d85f4660a2cb0f2aa2d665
UzomaAkpala <francisakpala@yahoo.com>
2023-11-13 14:39:41 +0100

    using middleware to set up server for static files

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require("express");
-
 const app = express();
+const PORT = 5000;
+const path = require("path");
+// setup static and middleware
 
-app.get("/", (req, res) => {
-  res.status(200).send("Home page");
-});
+app.use(express.static("./public"));
+// static assets are files the browser doesn't need to change
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+  res.status(404).send("Resource not found");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000...");
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
 });
