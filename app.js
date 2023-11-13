8a24f3056a34f0834a03c41c6cd24bfe5acb7819
UzomaAkpala <francisakpala@yahoo.com>
2023-11-13 18:09:14 +0100

    middleware continued

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const express = require("express");
 
 const app = express();
+const logger = require("./logger");
 
 // req => middleware => res
+app.use(logger);
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
-
-app.get("/", logger, (req, res) => {
+app.get("/", (req, res) => {
   res.send("Home Page");
 });
 
-app.get("/about", logger, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("About Page");
 });
 
