756556a893ab7c1bc63dbe445e67cc757ec3d0e5
UzomaAkpala <francisakpala@yahoo.com>
2023-11-14 11:27:31 +0100

    post method in express

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
-
 const app = express();
-const logger = require("./logger");
 
-// req => middleware => res
-app.use(logger);
+let { people } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
+//static assets
+app.use(express.static("./methods-public"));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
-app.get("/about", (req, res) => {
-  res.send("About Page");
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please provide credentials");
 });
 
 app.listen(5000, () => {
